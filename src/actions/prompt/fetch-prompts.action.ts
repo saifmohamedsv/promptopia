8efd9977382559f518a37bade8c35c05573b534a
saifmohamedsv <saifmohamed.dev@gmail.com>
2024-08-22 19:38:15 +0300

    feat: apply search logic

--- src/actions/prompt/fetch-prompts.action.ts
+++ src/actions/prompt/fetch-prompts.action.ts
@@ -2,8 +2,23 @@
 
 import prisma from "@/lib/db";
 
-export async function fetchPrompts() {
-  return await prisma.prompt.findMany({
+export async function fetchPrompts(searchText?: string) {
+  const searchCondition = searchText
+    ? {
+        OR: [
+          { prompt: { contains: searchText, mode: "insensitive" } },
+          { tag: { contains: searchText, mode: "insensitive" } },
+          {
+            creator: {
+              username: { contains: searchText, mode: "insensitive" },
+            },
+          },
+        ],
+      } 
+    : {};
+
+  return prisma.prompt.findMany({
+    where: searchCondition,
     include: {
       creator: true,
     },
